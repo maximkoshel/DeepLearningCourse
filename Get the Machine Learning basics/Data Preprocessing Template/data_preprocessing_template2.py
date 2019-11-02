46aeb8c8226966af3bd75ceaf677298d57bcd5af
maximkoshel <maximkoshel95@gmail.com>
2019-11-02 15:28:17 +0200

    maded template

--- Get the Machine Learning basics/Data Preprocessing Template/data_preprocessing_template2.py
+++ Get the Machine Learning basics/Data Preprocessing Template/data_preprocessing_template2.py
@@ -25,3 +25,13 @@ x = onehotencoder.fit_transform(x).toarray() #countries divided to separate colu
 
 labelencoder_y = LabelEncoder()
 y = labelencoder_y.fit_transform(y) #Tranform purchased column to 0,1
+
+#Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.2,random_state=0)
+
+#Feature scaling = numbers should be almost at the same range
+from sklearn.preprocessing import StandardScaler
+sc_x = StandardScaler()
+x_train = sc_x.fit_transform(x_train)
+x_test = sc_x.transform(x_test)
