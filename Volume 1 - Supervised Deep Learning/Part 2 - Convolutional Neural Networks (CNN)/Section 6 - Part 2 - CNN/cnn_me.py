744ad9408c10cc4e6525da89968add82707ee503
maximkoshel <maximkoshel95@gmail.com>
2019-11-18 18:13:01 +0200

    ,

---
+++ Volume 1 - Supervised Deep Learning/Part 2 - Convolutional Neural Networks (CNN)/Section 6 - Part 2 - CNN/cnn_me.py
@@ -0,0 +1,9 @@
+from keras.models import Sequential
+from keras.layers import Convolution2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.layers import Dense
+
+classifier = Sequential()
+
+classifier.add(Convolution2D(32,3,3))
