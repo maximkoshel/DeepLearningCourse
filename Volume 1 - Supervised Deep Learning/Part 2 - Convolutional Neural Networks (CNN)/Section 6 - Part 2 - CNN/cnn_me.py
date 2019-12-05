5c489eed704b03e9d5846f82145a87e66f8fe2c9
maximkoshel <maximkoshel95@gmail.com>
2019-12-05 21:07:52 +0200

    ,

--- Volume 1 - Supervised Deep Learning/Part 2 - Convolutional Neural Networks (CNN)/Section 6 - Part 2 - CNN/cnn_me.py
+++ Volume 1 - Supervised Deep Learning/Part 2 - Convolutional Neural Networks (CNN)/Section 6 - Part 2 - CNN/cnn_me.py
@@ -10,6 +10,9 @@ classifier = Sequential()
 #Convlolution
 classifier.add(Convolution2D(filters=32,kernel_size=[3,3],input_shape=(64,64,3),activation='relu'))
 
+#Adding second conv layer
+classifier.add(Convolution2D(filters=32,kernel_size=[3,3],activation='relu'))
+
 #Pooling
 classifier.add(MaxPooling2D(pool_size=(2,2)))
 
@@ -25,7 +28,7 @@ classifier.compile(optimizer='adam',loss= 'binary_crossentropy',metrics=['accura
 
 #Fitting the CNN to the images 
 
-from keras.preprocessing.image import ImageDataGenerator
+from keras.preprocessing.image import ImageDataGenerator #Image augmantation,confuses the system
 train_datagen = ImageDataGenerator(
         rescale=1./255,
         shear_range=0.2,
@@ -46,9 +49,46 @@ test_set = test_datagen.flow_from_directory(
         batch_size=32,
         class_mode='binary')
 
-classifier.fit_generator(
+classifier.fit_generator( 
         training_set,
         steps_per_epoch=8000,
         epochs=25,
         validation_data=test_set,
         validation_steps=2000)
+
+
+#Making single prediction 
+import numpy as np 
+from keras.preprocessing import image 
+test_image = image.load_img('dataset/single_prediction/cat_or_dog_2.jpg',target_size=(64, 64))
+test_image = image.img_to_array(test_image)#added the 3rd dimansion, line 11 (64,64,3)
+test_image = np.expand_dims(test_image,axis = 0)
+result = classifier.predict(test_image)
+training_set.class_indices #show what the paramters 
+if result [0][0] == 1:
+    prediction = 'dog'
+else:
+    prediction = 'cat'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
